3d9cc5bc5bc4407d97103a1f83371fbf77fd90f3
mutheunzyoka <nzyokagladys17@gmail.com>
2025-05-21 05:20:52 +0300

    week 7 assignment corrected

--- answers.sql
+++ answers.sql
@@ -1,20 +1,18 @@
 -- Question 1 Achieving 1NF (First Normal Form) 
 create table productdetail(
-productid int,
 orderid int,
 customername varchar (150),
 product varchar (200),
-quantity int,
-primary key(orderid,productid)
 );
-INSERT INTO productdetail (productid, orderid, customername, product, quantity)
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
 VALUES
-    (1, 101, 'John Doe', 'Laptop', 2),
-    (2, 101, 'John Doe', 'Mouse', 1),
-    (3, 102, 'Jane Smith', 'Tablet', 3),
-    (4, 102, 'Jane Smith', 'Keyboard', 1),
-    (5, 102, 'Jane Smith', 'Mouse', 2),
-    (6, 103, 'Emily Clark', 'Phone', 1);
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
 
     
 --Question 2 Achieving 2NF (Second Normal Form)
@@ -22,3 +20,21 @@ create table customer(
 orderid int primary key auto_increment,
 customername varchar (150)
 );
+
+-- Product  table 
+CREATE TABLE product(
+product_id INT primary key,
+productName varchar(100),
+quantity INT,
+order_id INT,
+foreign key(order_id) references orders(OrderID)
+);
+
+insert into product(product_id,productName,quantity,order_id)
+values 
+(1,'laptop',2,101),
+(2,'Mouse',1,101),
+(3,'Tablet',3,102),
+(4,'Keyboard',2,102),
+(5,'Mouse',1,102),
+(6,'Phone',1,103);
